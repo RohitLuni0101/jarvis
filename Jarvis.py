a0fe9a05fd7029ddeab83c782408808ae5e50065
RohitLuni0101 <78681041+RohitLuni0101@users.noreply.github.com>
2021-03-24 17:09:57 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -3,6 +3,10 @@ import datetime
 import webbrowser
 import wikipedia
 import os
+from requests import get
+import pywhatkit
+import smtplib
+import sys
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -27,6 +31,13 @@ def InputCommand():
     command = input("enter the command: ")
     return command
 
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gamil.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('your email id ', 'your password')
+    server.sendmail('your email id', to, content)
+    server.close()
 
 if __name__=="__main__":
         wishMe()
@@ -37,8 +48,9 @@ if __name__=="__main__":
                 webbrowser.open('http://www.youtube.com')
 
             elif 'open google' in query:
-                speak("opening google")
-                webbrowser.open('http://www.google.com')
+                speak("sir, what should i search on google")
+                cd = InputCommand().lower()
+                webbrowser.open(f"{cd}")
 
             elif 'open geeks' in query:
                 speak("opening geeksforgeeks")
@@ -46,23 +58,70 @@ if __name__=="__main__":
 
             elif 'open github' in query:
                 speak("opening Github")
-                webbrowser.open('https://github.com')
+                webbrowser.open('https://github.com/RohitLuni0101')
+
+            elif 'open stackoverflow' in query:
+                speak("opening Stackoverflow")
+                webbrowser.open('https://stackoverflow.com')
 
             elif 'open mail' in query:
                 speak("opening Gmail")
-                webbrowser.open('https://mail.google.com')
+                webbrowser.open('https://mail.google.com/mail/u/0/#inbox')
 
-            elif 'search' in query:
+            elif 'open notepad' in query:
+                speak('Opening Notepad')
+                Npath = "C:\\Windows\\system32\\notepad.exe"
+                os.startfile(Npath)
+
+            elif 'open control' in query:
+                speak('Opening control')
+                os.system("start control panel")
+
+            elif 'open command' in query:
+                speak('Opening command prompt')
+                os.system("start cmd")
+
+            elif 'open pc' in query:
+                speak('Opening This Pc')
+                os.system("start This PC")
+
+            elif 'wikipedia' in query:
                 speak("Searching wikipedia....")
-                query = query.replace("search", "")
-                results = wikipedia.summary(query, sentences=10)
+                query = query.replace("wikipedia", "")
+                results = wikipedia.summary(query, sentences=2)
                 speak("According to Wikipedia")
                 speak(results)
 
+
             elif 'play music' in query:
-                music_dir ='E:\Music\Genral Music'
+                music_dir ="E:\Music\Genral Music"
                 song = os.listdir(music_dir)
                 os.startfile(os.path.join(music_dir, song[11]))
                 speak("playing music")
-                print(song[11])
+
+            elif 'ip address' in query:
+                ip = get('https://api.ipify.org').text
+                speak(f'Your Ip Address is {ip}')
+
+            elif 'search youtube' in query:
+                speak("sir, what should i search on youtube")
+                query = InputCommand().lower()
+                pywhatkit.playonyt(query)
+
+            elif 'email to rohit' in query:
+                try:
+                    speak("sir, what should i say?")
+                    content = InputCommand().lower()
+                    to = "email id"
+                    sendEmail(to, content)
+                    speak("Email has been sent to ")
+                except Exception as e:
+                    print(e)
+                    speak("sorry sir, i am not able to sent this mail to rohit")
+
+            elif 'no thanks' in query:
+                speak("thanks for using me sir, have a good day.")
+                sys.exit()
+
+            speak("sir, do you have any other work")
 
